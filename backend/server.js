6b1284e64510604dc25a2b8a84b1ab0fad0380f1
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-01 08:45:57 +0330

    Add a welcome route with a JSON response

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require("express");
 // express app
 const app = express();
 
+// routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the app" });
+});
+
 // listen for requests
 app.listen(5000, () => {
   console.log("Listening on port 5000");
