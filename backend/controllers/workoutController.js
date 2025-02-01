2b08f6c2f6a63e8df6f61e4fa766cb97773a1bc1
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-01 16:47:40 +0330

    Controllers

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Workout = require("../models/workoutModel");
+
+// get all workouts
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(workouts);
+};
+
+// get a single workout
+const getWorkout = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.isValidObjectId(id)) {
+      return res.status(400).json({ error: "Invalid workout ID format" });
+    }
+
+    const workout = await Workout.findById(id);
+    if (!workout) {
+      return res.status(404).json({ error: "Workout not found" });
+    }
+
+    res.status(200).json(workout);
+  } catch (error) {
+    console.error("Error fetching workout:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// create a new workout
+const createWorkout = async (req, res) => {
+  const { title, reps, load } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a workout
+
+// update a workout
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+};
